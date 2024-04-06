b537833a569de4495b964517f28c7405cc8ac158
samuelhetes <samuel9.he@gmail.com>
2024-04-07 00:16:52 +0200

    main page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="fadeIn-short bg-black text-white px-40 py-2 h-32">
+        <div class="w-full h-full flex space-x-8">
+            <div class="flex h-full items-center">
+                <img src="../assets/fig/short-logo.png"
+                width="100">
+            </div>
+            <div class="h-full flex items-center space-x-6 py-2">
+                <div class="h-full text-xl hover:border-b">Investície</div>
+                <div class="text-xl">Transakcie</div>
+                <div class="text-xl">Vzdelávanie</div>
+                <div class="text-xl">Profil</div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 
 import ChatBot from "@/components/ChatBot.vue";
+import NavBar from "@/components/NavBar.vue";
+
 </script>
 
 <template>
-  <div class="flex justify-center h-screen w-full flex-wrap" style="background-color: #1d1e23">
-    <ChatBot class="absolute right-0 bottom-0 m-4"></ChatBot>
+  <div class="bg-main_color w-full h-screen">
+    <NavBar/>
   </div>
+  <ChatBot class="absolute right-0 bottom-0 m-4"></ChatBot>
 </template>
